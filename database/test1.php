ffb477469d658083c26e19da261e4adb1c11917e
malith94batugedara <malithbatugedara@gmail.com>
2023-11-01 18:50:25 +0530

    project added

---
+++ database/test1.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "database.php";
+
+$dbo=new Database();
+$cmd="select * from department_details";
+
+$statement=$dbo->conn->prepare($cmd);
+$statement->execute();
+$rv=$statement->fetchAll(PDO::FETCH_ASSOC);
+print_r($rv);
+?>
